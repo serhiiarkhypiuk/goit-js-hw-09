c864038bd66003e2ea197b96df939db87cf6ad33
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-05-25 18:39:39 +0300

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,57 @@
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector("body")
+
+const startButton = document.querySelector("button[data-start]")
+const stopButton = document.querySelector("button[data-stop]")
+let interval
+
+const resetButton = document.createElement("button")
+resetButton.textContent = 'Reset'
+resetButton.type = 'button'
+resetButton.setAttribute('data-reset','')
+
+stopButton.after(resetButton)
+
+stopButton.disabled = true
+resetButton.disabled = true
+
+const changeColor = () => {
+    interval = setInterval(() => {
+        const color = getRandomHexColor()
+
+        body.style.backgroundColor = color
+    }, 1000)
+}
+
+const resetColor = () => {
+    body.style.backgroundColor = '#FFFFFF'
+}
+
+const disableButton = (button) => {
+    button.disabled = true
+}
+
+const enableButton = (button) => {
+    button.disabled = false
+}
+
+startButton.addEventListener('click', () => {
+    changeColor()
+    disableButton(startButton)
+    enableButton(stopButton)
+})
+
+stopButton.addEventListener('click', () => {
+    clearInterval(interval)
+    enableButton(startButton)
+    disableButton(stopButton)
+    enableButton(resetButton)
+})
+
+resetButton.addEventListener('click', () => {
+    resetColor()
+    disableButton(resetButton)
+})
